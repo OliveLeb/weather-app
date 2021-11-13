200a59816443e09c3e7400755e3e379fb7fcb597
OliveLeb <lebel.olivier@wanadoo.fr>
2021-11-13 03:31:10 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+const API_KEY = 'c9dc3570b9acad70c7bd636b91c1495e';
+
+</script>
+
+<template>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
