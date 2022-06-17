1728f0842a51b383c50584de81b2ec034fed76d5
OliveLeb <lebel.olivier@wanadoo.fr>
2022-06-18 00:58:39 +0200

    add unocss

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
 import path from 'path'
+import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias: {
-    'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js',
+  resolve: {
+    alias: {
+      'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js',
+    },
   },
   plugins: [
     vue(),
     vueI18n({
       include: path.resolve(__dirname, 'src/locales/**'),
     }),
+    Unocss()
   ],
 })
